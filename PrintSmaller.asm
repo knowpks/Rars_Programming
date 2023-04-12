34364b6211c8c576ced53f984c088d6c75c3222b
Prasanta K S <121389191+knowpks@users.noreply.github.com>
2023-04-12 14:54:04 +0200

    Update PrintSmaller.asm

--- PrintSmaller.asm
+++ PrintSmaller.asm
@@ -2,23 +2,23 @@
 # (in the data segment), print the smaller.
 
 .data 
-	.word 0
-	.word 10
+		.word 0
+		.word 10
 .text
-	lui t0, 0x10010
-	lw  t1, 0(t0)
-	lw  t2, 4(t0)
-	# if t1 < t2 print t1
-	blt t1, t2, print 
-	addi a0, t2, 0
-	addi a7, zero, 1
-	ecall
+		lui t0, 0x10010
+		lw  t1, 0(t0)
+		lw  t2, 4(t0)
+		# if t1 < t2 print t1
+		blt t1, t2, print 
+		addi a0, t2, 0
+		addi a7, zero, 1
+		ecall
 	
-	beq zero, zero, exit
-	# else: print t2
+		beq zero, zero, exit
+		# else: print t2
 print:	addi a0, t1, 0
-	addi a7, zero, 1
-	ecall	
+		addi a7, zero, 1
+		ecall	
 	
 exit:	addi a7, zero, 10
-	ecall
+		ecall
